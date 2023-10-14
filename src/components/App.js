4580485e6b4999f51f647d3ed6d86c348fcc3264
Rohit <rs4787690@gmail.com>
2023-10-14 15:46:37 +0530

    degugging

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+
+const App = () => {
+  let [count, setCount] = useState(0);
+
+  return (
+    <div className="ball">
+      <h1 className="count" ondoubleclick={() => alert("cant edit it")}>
+        {count}
+      </h1>
+      <button className="increment-button" onclick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default App;
